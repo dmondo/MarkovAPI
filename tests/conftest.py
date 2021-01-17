54d4b07d6b8ef8fdc933cbc319c7ebcebdd58e9a
dmondo <dannysmondo@gmail.com>
2021-01-17 11:11:23 -0800

    init(pytest): pytest and config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from src.markov_api import app
 
+
 @pytest.fixture
 def client():
     return app.app.test_client()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,4 @@
 def test_app_route(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'hello world'
+    assert response.data.decode("utf-8") == "hello world"
