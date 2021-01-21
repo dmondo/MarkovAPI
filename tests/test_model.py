a9a73c4cd81ee024d414b75ef8f12ec774c63393
dmondo <dannysmondo@gmail.com>
2021-01-20 22:54:29 -0800

    tests(markov): test endpoint and helper functions for markov chains

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,7 @@
 """Markov model tests."""
 from unittest import TestCase
 
-from src.helpers.markov import build_model
+from src.helpers.markov import build_model, generate_chain
 
 
 def test_build_model() -> None:
@@ -15,3 +15,21 @@ def test_build_model() -> None:
         "some sample": ["text"],
     }
     TestCase().assertDictEqual(model, expected_model)
+
+
+def test_generate_chain() -> None:
+    """It generates a markov chain from a given model."""
+    test_data = ["This is some is some sample text"]
+    model = build_model(test_data, 2)
+    chain = generate_chain(model=model, length=3, order=2)
+    assert len(chain.split(" ")) >= 2
+
+    test_data = ["blah blah blah blah"]
+    model = build_model(test_data, 1)
+    chain = generate_chain(model=model, length=5, order=1)
+    assert chain == "blah blah blah blah blah"
+
+    test_data = ["no possibilities"]
+    model = build_model(test_data, 1)
+    chain = generate_chain(model=model, length=3, order=1)
+    assert len(chain.split(" ")) <= 2
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,24 @@
 """API route tests."""
+import json
+from unittest import TestCase
+
 from flask.testing import FlaskClient
 
 
-def test_app_route_returns_model(client: FlaskClient) -> None:
+def test_app_model_route_returns_model(client: FlaskClient) -> None:
     """It returns 200 and a markov model object."""
     response = client.get("/model")
+    data = json.loads(response.get_data())
+    expected_model = {"is some sample": ["text"], "this is some": ["sample"]}
+
     assert response.status_code == 200
+    TestCase().assertDictEqual(data, expected_model)
+
+
+def test_app_chain_route_returns_chain(client: FlaskClient) -> None:
+    """It returns 200 and a markov chain object."""
+    response = client.get("/chain")
+    data = json.loads(response.get_data())
+
+    assert response.status_code == 200
+    assert isinstance(data, str)
