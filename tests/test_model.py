27c67cd3ec510e68ee5e51c585918eb3202511e2
dmondo <dannysmondo@gmail.com>
2021-01-24 17:29:55 -0800

    test(project): update tests for chains and routes

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,26 @@
 """Markov model tests."""
 from unittest import TestCase
 
-from src.helpers.markov import build_model, generate_chain
+from src.helpers import markov
 
 
 def test_build_model() -> None:
     """It constructs a dictionary representing a markov model from text."""
     test_data = ["This is some is some sample text"]
-    model = build_model(test_data, 2)
+    model = markov.build_model(test_data, 2)
+    expected_model = {
+        "This is": ["some"],
+        "is some": ["is", "sample"],
+        "some is": ["some"],
+        "some sample": ["text"],
+    }
+    TestCase().assertDictEqual(model, expected_model)
+
+
+def test_model_removes_links() -> None:
+    """Build_model removes http link from text before building."""
+    test_data = ["This is some is some sample text https://fake_url.com"]
+    model = markov.build_model(test_data, 2)
     expected_model = {
         "This is": ["some"],
         "is some": ["is", "sample"],
@@ -20,16 +33,35 @@ def test_build_model() -> None:
 def test_generate_chain() -> None:
     """It generates a markov chain from a given model."""
     test_data = ["This is some is some sample text"]
-    model = build_model(test_data, 2)
-    chain = generate_chain(model=model, length=3, order=2)
+    model = markov.build_model(test_data, 2)
+    chain = markov.generate_chain(model=model, length=3, order=2)
     assert len(chain.split(" ")) >= 2
 
     test_data = ["blah blah blah blah"]
-    model = build_model(test_data, 1)
-    chain = generate_chain(model=model, length=5, order=1)
+    model = markov.build_model(test_data, 1)
+    chain = markov.generate_chain(model=model, length=5, order=1)
     assert chain == "blah blah blah blah blah"
 
     test_data = ["no possibilities"]
-    model = build_model(test_data, 1)
-    chain = generate_chain(model=model, length=3, order=1)
+    model = markov.build_model(test_data, 1)
+    chain = markov.generate_chain(model=model, length=3, order=1)
     assert len(chain.split(" ")) <= 2
+
+
+def test_generate_chains() -> None:
+    """It generates chains up to the retry limit by calling generate_chain."""
+    test_data = ["This is some is some sample text"]
+    model = markov.build_model(test_data, 2)
+    num_chains = 5
+    chains = markov.generate_chains(
+        model=model, length=2, order=2, num_chains=num_chains
+    )
+    assert len(chains) == num_chains
+
+    test_data = ["This chain will never reach the desired length"]
+    model = markov.build_model(test_data, 2)
+    num_chains = 1
+    chains = markov.generate_chains(
+        model=model, length=15, order=2, num_chains=num_chains
+    )
+    assert len(chains) == num_chains
