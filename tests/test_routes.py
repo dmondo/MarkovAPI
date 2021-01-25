582cc68791d09c0f807d6f14845bcd56886bf023
dmondo <dannysmondo@gmail.com>
2021-01-24 17:29:35 -0800

    test(project): update tests for chains and routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,24 +1,17 @@
 """API route tests."""
-import json
-from unittest import TestCase
-
 from flask.testing import FlaskClient
-
-
-def test_app_model_route_returns_model(client: FlaskClient) -> None:
-    """It returns 200 and a markov model object."""
-    response = client.get("/model")
-    data = json.loads(response.get_data())
-    expected_model = {"is some sample": ["text"], "this is some": ["sample"]}
-
-    assert response.status_code == 200
-    TestCase().assertDictEqual(data, expected_model)
 
 
 def test_app_chain_route_returns_chain(client: FlaskClient) -> None:
     """It returns 200 and a markov chain object."""
-    response = client.get("/chain")
-    data = json.loads(response.get_data())
+    payload = {
+        "text": ["This is some sample text"],
+        "order": 3,
+        "length": 10,
+        "num_chains": 2,
+    }
+    response = client.post("/chains", json=payload)
+    data = response.get_json()
 
     assert response.status_code == 200
-    assert isinstance(data, str)
+    assert isinstance(data, list)
